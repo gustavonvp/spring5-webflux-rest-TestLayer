52b2d55c2821b2780c4372e3055ab3f9d8cee62a
gustavonvp <gustavonvp@gmail.com>
2021-08-31 13:11:12 -0300

    Adding Controller Customer  Closes #15

--- src/main/java/guru/springframework/spring5webfluxrest/domain/Customer.java
+++ src/main/java/guru/springframework/spring5webfluxrest/domain/Customer.java
@@ -1,5 +1,8 @@
 package guru.springframework.spring5webfluxrest.domain;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -8,6 +11,9 @@ import javax.persistence.Id;
 
 @Entity
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Customer {
 
     @Id
